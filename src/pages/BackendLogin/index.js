9fab217a485350632745858a1fcba215a89f91c8
qh70 <qh7047789@gmail.com>
2023-04-05 23:09:13 +0800

    backendlogin

--- src/pages/BackendLogin/index.js
+++ src/pages/BackendLogin/index.js
@@ -7,22 +7,23 @@ const index = () => {
 
         </div>
         <div className="rightInBackendLogin">
+          <div className="blockInRightInBackendLogin">
             <div className="picNBeThingInBackendLogin">
-                <img className="logoInBackendLogin" src={require("../../static/1.png")}/>
-                <div></div>
-            </div>
-            <div>Sign in to Dashboard</div>
-            <div>Email/ User Name</div>
-            <input></input>
-            <div>Password</div>
-            <input></input>
-            <div>Forgot password？</div>
-            <button></button>
-            <div>
-                <div></div>
-                <div>Remember me</div>
-            </div>
-            
+                  <img className="logoInBackendLogin" src={require("../../static/1.png")}/>
+                  <img className="beThingInBackendLogin" src={require("../../static/BE-Thing.png")}></img>
+              </div>
+              <div className="wordSignInBackendLogin">Sign in to Dashboard</div>
+              <div className="wordEmailInBackendLogin">Email/ User Name</div>
+              <input className="inputEmailInBackendLogin" placeholder="input your email/ user name in here"></input>
+              <div className="wordEmailInBackendLogin">Password</div>
+              <input className="inputPasswordInBackendLogin" placeholder="input your password here"></input>
+              <div className="wordforgotInBackendLogin">Forgot password？</div>
+              <button className="buttonInBackendLogin">Sign</button>
+              <div>
+                  <div></div>
+                  <div>Remember me</div>
+              </div>
+          </div>
         </div>      
     </div>
   )
